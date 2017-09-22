85807e37aff75566da9d374f75d5e342f3956112
Yuwen Chang <dd20085@gmail.com>
2017-09-22 16:20:31 -0400

    submit HW 2-1 & 2-2

--- HW2_ywc249/get_bus_info_ywc249.py
+++ HW2_ywc249/get_bus_info_ywc249.py
@@ -3,9 +3,9 @@
 # Code written for HW2 of PUI2017
 # https://github.com/ywnch/PUI2017_ywc249/HW2_ywc249
 ##############################
-# put MTA API key and bus route as input arguments:
+# put MTA API key, bus route, and output csv file as input arguments:
 # i.e. run the code as:
-# 	python show_bus_locations_ywc249.py <MTA_KEY> <BUS_LINE>
+# 	python get_bus_info_ywc249.py <MTA_KEY> <BUS_LINE> <BUS_LINE>.csv
 ##############################
 
 # ensure compatibility and allow reading line input args
@@ -13,8 +13,8 @@ from __future__ import print_function
 import sys
 
 # check input args
-if not len(sys.argv) == 3:
-    print ("Invalid number of arguments. Run as: python show_bus_locations_ywc249.py <MTA_KEY> <BUS_LINE>")
+if not len(sys.argv) == 4:
+    print ("Invalid number of arguments. Run as: python get_bus_info_ywc249.py <MTA_KEY> <BUS_LINE> <BUS_LINE>.csv")
     sys.exit()
 
 # import packages
@@ -25,10 +25,12 @@ except ImportError:
 
 import os
 import json
+import pandas as pd
 
 # read args and generate url to fetch data from
 apikey = sys.argv[1]
 bus = sys.argv[2]
+file = sys.argv[3]
 url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(apikey, bus)
 #print("API URL: \n" + url)
 
@@ -45,16 +47,37 @@ except:
     print(error['Description'])
     sys.exit()
 
-# parse route info: line number, active vehicles
-print("Bus Line: " + bus)
-print("Number of Active Buses: " + str(len(data2)))
+# parse info
+lat = []
+lng = []
+stop = []
+status = []
 
-# parse vehicle info: location (lat, long)
-i = 1
 for v in data2:
+    #global lat, lng, stop, status
+
+    # parse location
     lt = v['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
-    lng = v['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-    vrf = v['MonitoredVehicleJourney']['VehicleRef']
-    vrf = vrf.split("_")[1]
-    print("Bus %s (#%s) is at latitude %s and longitude %s"%(i, vrf, lt, lng))
-    i += 1
+    lg = v['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+    lat.append(lt)
+    lng.append(lg)
+
+    # parse next stop info
+    if 'OnwardCall' in v['MonitoredVehicleJourney']['OnwardCalls']:
+        s = v['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]
+        stt = s['Extensions']['Distances']['PresentableDistance']
+        stp = s['StopPointName']
+    else:
+        stt = 'N/A'
+        stp = 'N/A'
+    status.append(stt)
+    stop.append(stp)
+
+# write data to dictionary, indexing from 1
+d = {'Latitude': lat, 'Longitude': lng, 'Stop Name': stop, 'Stop Status': status}
+df = pd.DataFrame(data = d)
+df.index += 1
+
+# write df to csv file
+with open('%s'%(file), 'w') as f:
+    df.to_csv('%s'%(file))
